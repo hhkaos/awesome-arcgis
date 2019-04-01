91a77b156c5aabb389aa2b96228171a8226d4433
RaulJimenezOrtega <hhkaos@gmail.com>
2019-04-01 08:02:56 +0200

    Adding snippet to sync-product-items.js

--- sync-product-items.js
+++ sync-product-items.js
@@ -3,6 +3,8 @@ require('isomorphic-fetch')
 import { searchItems, updateItem } from '@esri/arcgis-rest-items';
 import { UserSession } from '@esri/arcgis-rest-auth';
 import { parse } from 'node-html-parser';
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 
 var fs = require('fs');
@@ -32,21 +34,22 @@ searchItems({
     }).then(response => {
     console.log("response.results.length=",response.results.length);
     response.results.forEach((elem, i) =>{
-        //if(elem.id=='08200c13cf5f4ba29097d35dbc96d9b3'){
-            parseHTML(elem.url).then(function(html) {
+        // if(elem.id=='08200c13cf5f4ba29097d35dbc96d9b3'){
+            parseHTML(elem.url).then(function(obj) {
                 updateItem({
-                  item: {
-                    id: elem.id,
-                    description: html,
-                    licenseInfo: `<div style="text-align: left;">Content is licensed under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">Attribution 4.0 International (CC BY 4.0)</a></div><div style="text-align: left;">Source code under <a href="https://github.com/esri-es/awesome-arcgis/blob/master/LICENSE" target="_blank">GNU GENERAL PUBLIC LICENSE</a><br /></div>`,
-                    accessInformation: 'https://github.com/esri-es/awesome-arcgis/graphs/contributors'
-                  },
-                  authentication: session
-              }).then(response=>{
-                  console.log("response=",response)
-              });
+                    item: {
+                        id: elem.id,
+                        description: obj.html,
+                        licenseInfo: `<div style="text-align: left;">Content is licensed under <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">Attribution 4.0 International (CC BY 4.0)</a></div><div style="text-align: left;">Source code under <a href="https://github.com/esri-es/awesome-arcgis/blob/master/LICENSE" target="_blank">GNU GENERAL PUBLIC LICENSE</a><br /></div>`,
+                        accessInformation: 'https://github.com/esri-es/awesome-arcgis/graphs/contributors',
+                        snippet: obj.summary
+                    },
+                    authentication: session
+                }).then(response=>{
+                    console.log("response=",response)
+                });
             });
-        //}
+        // }
 
     })
 });
@@ -56,17 +59,23 @@ function parseHTML(url){
     var path = url.split("/").slice(4).join("/");
     var promise = new Promise(function(resolve, reject) {
         fs.readFile(`docs/${path}index.html`, 'utf8', function(err, html) {
+
             try{
                 var site = url,
                     root = parse(html),
                     content = root.querySelector('.search-noresults').toString(),
-                    content = urlify(content, site);
+                    content = urlify(content, site),
                     content = headify(content);
 
-                resolve(content);
+                // Forr some reason I couldn't make parse work to get the summary
+                const dom = new JSDOM(content);
+                var summary = dom.window.document.querySelector('.search-noresults p:nth-child(3)').textContent;
+
+                resolve({html:content, summary: summary});
             }catch(e){
+                console.log("Error: ",e);
                 console.log("\nurl=",url);
-                console.log("html=",html);
+                //console.log("html=",html);
             }
 
         });
@@ -89,11 +98,11 @@ function headify(text) {
         var result = str.match(/(<h\d) (.*)>(.*)(<\/h\d>)/);
         switch(result[1]){
             case '<h1':
-                return `<font size="6" ${result[2]}>${result[3]}</font>`;
+                return `<p><font size="6" ${result[2]}>${result[3]}</font></p>`;
             case '<h2':
-                return `<font size="5" ${result[2]}>${result[3]}</font>`;
+                return `<p><font size="5" ${result[2]}>${result[3]}</font></p>`;
             default:
-                return `<font size="4" ${result[2]}>${result[3]}</font>`;
+                return `<p><font size="4" ${result[2]}>${result[3]}</font></p>`;
         }
     })
 }
